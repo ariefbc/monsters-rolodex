2199d30dbbaa04fde3674237054db54c0ef130d2
Arief Setiawan <ariefbc@gmail.com>
2021-09-28 10:06:03 +0700

    adding files for github pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./search-box.styles.css";
+
+export const SearchBox = ({ placeholder, searchHandler }) => {
+  return (
+    <input
+      className="search"
+      type="search"
+      placeholder={placeholder}
+      onChange={searchHandler}
+    />
+  );
+};
